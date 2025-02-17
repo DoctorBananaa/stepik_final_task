fd4cf71c628f13335bea99ec4ba90cf4c0f02ba7
Julia Kamysheva <jkamysheva@gmail.com>
2025-02-17 17:42:01 +0300

    добавлены три теста проверки отсутствия элемента

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,6 @@
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException, TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class BasePage:
     def __init__(self, browser, url, timeout=10):
@@ -31,3 +32,18 @@ class BasePage:
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+    def is_not_element_present(self, how, what, timeout=4): #упадет, если элем появится
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+    def is_disappeared(self, how, what, timeout=4): #ждет, пока элем не пропадет
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,5 +1,8 @@
 from ..pages.product_page import ProductPage
+from ..pages.locators import ProductPageLocators
 import pytest
+
+LINK = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -12,10 +15,29 @@ import pytest
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_basket(browser, link):
-    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
     assert page.take_product_name() == page.take_product_name_in_basket(), 'Имя не совпадает'
     assert page.take_product_price() == page.take_product_price_in_basket(), 'Стоимость не совпадает'
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, LINK)
+    page.open()
+    page.add_to_basket()
+    assert page.is_not_element_present(*ProductPageLocators.PRODUCT_PRICE_BASKET),\
+        'Сообщения про новую стоимость корзины не появилось'
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, LINK)
+    page.open()
+    assert page.is_not_element_present(*ProductPageLocators.PRODUCT_PRICE_BASKET), \
+        'Сообщения про новую стоимость корзины не появилось'
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, LINK)
+    page.open()
+    page.add_to_basket()
+    assert page.is_disappeared(*ProductPageLocators.PRODUCT_PRICE_BASKET),\
+        'Сообщение об успехе не пропало'
