ed2af873fd683df8d8820b90ed1bde569f959792
Julia Kamysheva <jkamysheva@gmail.com>
2025-05-13 11:59:22 +0300

    убрала опечатки, вынесла ссылки в отдельный файл

---
+++ pages/links.py
@@ -0,0 +1,6 @@
+class MainPageLinks:
+    PRODUCT_LINK = 'https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/'
+    MAIN_PAGE_LINK = 'http://selenium1py.pythonanywhere.com/ru/'
+
+class ProductPageLinks:
+    PRODUCT_LINK = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/'
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -3,12 +3,12 @@ from ..pages.main_page import MainPage
 from ..pages.login_page import LoginPage
 from ..pages.basket_page import BasketPage
 from ..pages.locators import BasketPageLocators
+from ..pages.links import MainPageLinks
 
 
 class TestLoginFromMainPage:
     def test_guest_can_go_to_login_page(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
-        page = MainPage(browser, link)
+        page = MainPage(browser, MainPageLinks.PRODUCT_LINK)
         page.open()
         page.should_be_login_link()
         page.go_to_login_page()
@@ -16,8 +16,7 @@ class TestLoginFromMainPage:
         login_page.should_be_login_page()
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/"
-    page = MainPage(browser, link)
+    page = MainPage(browser, MainPageLinks.MAIN_PAGE_LINK)
     page.open()
     page.go_to_basket()
     basket_page = BasketPage(browser, browser.current_url)
